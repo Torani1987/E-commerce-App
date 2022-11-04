83621af0496510e95bff4248ecac584436db4dd8
Torani1987 <rahmattorani20@gmail.com>
2022-11-04 08:29:20 +0700

    Login page and add some properties for color

---
+++ lib/page/testing.dart
@@ -0,0 +1,13 @@
+import 'package:final_project/properties%20app/prop.dart';
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: color3,
+    );
+  }
+}
