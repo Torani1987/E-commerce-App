d5ed0c37b83711e86905c26f83298c31908d1f6b
Torani1987 <rahmattorani20@gmail.com>
2022-11-15 11:47:26 +0700

    Updating product model

--- lib/view_model/productServiceApi.dart
+++ lib/view_model/productServiceApi.dart
@@ -1,6 +1,21 @@
 import 'dart:convert';
+import 'package:final_project/model/ProductModel.dart';
 import 'package:http/http.dart' as http;
 
-class FetchLogin {
+class FetchProduct {
   var data = [];
+  List<Product> results = [];
+  String productUrl = 'https://api1.sib3.nurulfikri.com/api/barang';
+  Future<List<Product>> getRegisterList() async {
+    final response = await http.get(Uri.parse(productUrl));
+    try {
+      if (response.statusCode == 200) {
+        data = jsonDecode(response.body);
+        results = data.map((e) => Product.fromJson(e)).toList();
+      } else {
+        print('Api Error');
+      }
+    } catch (e) {}
+    return results;
+  }
 }
